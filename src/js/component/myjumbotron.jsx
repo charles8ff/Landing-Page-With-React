aa5bf29a3d27f0ed48d4f28f730209bd3f262ccc
Carlos Fisac Ferrández <charles8ff@gmail.com>
2021-01-25 16:33:43 +0000

    first jumbotron

---
+++ src/js/component/myjumbotron.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
+
+export const MyJumbotron = props => (
+	<Jumbotron className="theJumbo">
+		<div>
+			<h1>Enjoy your stay at The Book Basement </h1>
+			<p>
+				Greetings, tired traveller! Get comfy, grab a drink, and pick a
+				whole lot of books to get rolling into the fantasies only books
+				can behold.
+			</p>
+		</div>
+		<p>
+			<Button variant="warning">Learn more about us</Button>
+		</p>
+	</Jumbotron>
+);
