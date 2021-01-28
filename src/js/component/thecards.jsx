188a07bfbdbf18f9835607162dac8f8356327973
Rafaela Gonçalves <75177742+rafaelagcalves@users.noreply.github.com>
2021-01-28 12:45:41 +0100

    Delete comments

--- src/js/component/thecards.jsx
+++ src/js/component/thecards.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
 
-// export const cards = props => {
-// 	return cardsContents.map((singleCard, i) => (
-// 		<Card key={i}>
-// 			<Card.Img variant="top" src="holder.js/100px160" />
-// 			<Card.Body>
-// 				<Card.Title>{singleCard.Title}</Card.Title>
-// 				<Card.Text>
-// 					This is a wider card with supporting text below as a natural
-// 					lead-in to additional content. This content is a little bit
-// 					longer.
-// 				</Card.Text>
-// 			</Card.Body>
-// 			<Card.Footer>
-// 				<small className="text-muted">Last updated 3 mins ago</small>
-// 			</Card.Footer>
-// 		</Card>
-// 	));
-// };
 
 export const TheCards = props => {
 	console.log(props);
